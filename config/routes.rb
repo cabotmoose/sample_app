fa2b123100ed660a951e536837a23fbbb29ad762
Cameron Smith <cabotmoose@gmail.com>
2014-03-07 20:12:23 -0500

    sdf

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ SampleApp::Application.routes.draw do
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/fbtest',  to: 'static_pages#fbtest',  via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
